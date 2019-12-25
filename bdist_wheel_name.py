b339360153dfc24075ff20f6712c76565a2384ad
John Vandenberg <jayvdb@gmail.com>
2019-12-25 10:39:48 +0700

    Remove unnecessary 'pass'

--- bdist_wheel_name.py
+++ bdist_wheel_name.py
@@ -3,7 +3,6 @@ from wheel.bdist_wheel import bdist_wheel
 
 class bdist_wheel_name(bdist_wheel):
     def run(self):
-        pass
         impl_tag, abi_tag, plat_tag = self.get_tag()
         archive_basename = "{}-{}-{}-{}".format(
             self.wheel_dist_name, impl_tag, abi_tag, plat_tag
